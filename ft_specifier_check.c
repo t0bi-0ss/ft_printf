a5f61c5a6a96d1a56f75d049fe3c0670d8ae247c
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-05-08 15:07:30 +0200

    Update

--- ft_specifier_check.c
+++ ft_specifier_check.c
@@ -6,13 +6,13 @@
 /*   By: tsordo-o <tsordo-o@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 18:25:04 by tsordo-o          #+#    #+#             */
-/*   Updated: 2026/05/06 18:33:26 by tsordo-o         ###   ########.fr       */
+/*   Updated: 2026/05/08 15:05:58 by tsordo-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_specifier_check(char c)
+int	ft_specifier_check(char *specifier_flags, char c)
 {
-	return (ft_strchr("cspdiuxX%%", c));
+	return (ft_strchr(specifier_flags, c) != NULL);
 }
