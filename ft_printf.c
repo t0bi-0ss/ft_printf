a4c69e9bfbd7e08380c4780fa8966a6eb37e64fe
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-05-08 15:07:09 +0200

    New function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tsordo-o <tsordo-o@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 18:43:01 by tsordo-o          #+#    #+#             */
-/*   Updated: 2026/05/06 19:24:04 by tsordo-o         ###   ########.fr       */
+/*   Updated: 2026/05/08 15:05:57 by tsordo-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,34 @@
 
 int	ft_printf(char const *format, ...)
 {
-	va_list args;
+	va_list	args;
+	int		result;
+	char	*specifier_flags;
 
+	result = 1;
+	specifier_flags = "cspdiuxX%%";
 	va_start(args, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (!ft_specifier_check(*format))
-				return (0);
+			if (!ft_specifier_check(specifier_flags, *format))
+			{
+				va_end(args);
+				result = -1;
+				return (result);
+			}
+			result = ft_flag_checker(args, *format++);
+			if (result < 0)
+			{
+				va_end(args);
+				return (result);
+			}
 		}
+		result = ft_new_putchar_fd(*format, 1);
+		format++;
 	}
+	va_end(args);
+	return (result);
 }
