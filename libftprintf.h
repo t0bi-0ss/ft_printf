eb2e8a0854b524ca654eb7cc26453dbea1318055
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-05-08 15:08:07 +0200

    Update

--- libftprintf.h
+++ libftprintf.h
@@ -1,22 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsordo-o <tsordo-o@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 12:37:40 by tsordo-o          #+#    #+#             */
-/*   Updated: 2026/05/06 18:14:02 by tsordo-o         ###   ########.fr       */
+/*   Updated: 2026/05/08 15:06:00 by tsordo-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
-# include <stdarg.h>
-# include <unistd.h>
 # include "libft/libft.h"
+# include <stdarg.h>
+# include <stdint.h>
+# include <unistd.h>
 
-int	ft_specifier_check(char c);
-int ft_printf(char const *, ...);
+char	ft_to_hex(unsigned char quotient);
+int		ft_new_putchar_fd(char c, int fd);
+int		ft_new_putstr_fd(char *s, int fd);
+int		ft_specifier_check(char *specifier_flags, char c);
+int		ft_flag_checker(va_list args, char flag);
+int		ft_void_to_hex(uintptr_t address);
+int		ft_printf(char const *, ...);
 
 #endif
