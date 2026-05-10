78a1ae16d6dbb077e2691366e69ab2d436cce197
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-05-10 16:12:49 +0200

    Change library header used

--- ft_new_putchar_fd.c
+++ ft_new_putchar_fd.c
@@ -6,11 +6,11 @@
 /*   By: tsordo-o <tsordo-o@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 14:23:56 by tsordo-o          #+#    #+#             */
-/*   Updated: 2026/05/09 19:41:19 by tsordo-o         ###   ########.fr       */
+/*   Updated: 2026/05/10 16:12:37 by tsordo-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include <unistd.h>
 
 int	ft_new_putchar_fd(char c, int fd)
 {
